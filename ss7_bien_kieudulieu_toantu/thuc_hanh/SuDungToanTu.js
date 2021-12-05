f44168dfcbee2e11f06346178f2549c23f94a779
C1121G1-LE-VAN-CHIEN <chienle26897@gmail.com>
2021-12-06 00:34:57 +0700

    Update BT

---
+++ ss7_bien_kieudulieu_toantu/thuc_hanh/SuDungToanTu.js
@@ -0,0 +1,6 @@
+let inputwidth = prompt('Enter the width');
+let width = parseInt(inputwidth);
+let inputheight = prompt('Enter the height');
+let height = parseInt(inputheight);
+let area = width * height;
+document.write("The area is: " + area);
