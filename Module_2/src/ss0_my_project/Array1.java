1bad14ad7bfd55f8037547f4c97a0eb9a13f2466
C1121G1-LE-VAN-CHIEN <chienle26897@gmail.com>
2021-12-31 16:25:27 +0700

    Update BT

---
+++ Module_2/src/ss0_my_project/Array1.java
@@ -0,0 +1,11 @@
+package ss0_my_project;
+
+import java.util.Arrays;
+
+public class Array1 {
+    public static void main(String[] args) {
+        String[] a = {"Jonh", "Mary", "Caly", "Tomy"};
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
+    }
+}
