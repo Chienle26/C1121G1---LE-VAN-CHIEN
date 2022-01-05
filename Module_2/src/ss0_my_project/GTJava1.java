74166be1826bee7be75e988c3a7e8d5782f6c2b4
C1121G1-LE-VAN-CHIEN <chienle26897@gmail.com>
2022-01-05 14:23:05 +0700

    Update BT

--- Module_2/src/ss0_my_project/GTJava1.java
+++ Module_2/src/ss0_my_project/GTJava1.java
@@ -1,29 +1,25 @@
 package ss0_my_project;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class GTJava1 {
     public static void main(String[] args) {
-        int[] arr1 = {7, 2, 5, 3, 5, 3};
-        int[] arr2 = {7, 2, 5, 4, 6, 3, 5, 3};
-        int[] arr3 = new int[arr1.length];
-        boolean check;
-        int index = 0;
-        int j;
-        for (int i = 0; i < arr2.length; i++) {
-            check = true;
-            for (j = 0; j < arr1.length; j++) {
-                if (arr2[j] == arr1[i]) {
-                    check = false;
+        int[] a = {7, 2, 5, 3, 5, 3};
+        int[] b = {7, 2, 5, 4, 6, 3, 5, 3, 4, 6};
+        Stack<Integer> c = new Stack<>();
+        for (int i = 0; i < b.length; i++) {
+            boolean flag = true;
+            for (int j = 0; j < a.length; j++) {
+                if (a[j] == b[i]) {
+                    flag = false;
                     break;
                 }
             }
-            if (check) {
-
-                arr3[index] = arr2[i];
-                index+=1;
+            if (flag) {
+                c.push(b[i]);
             }
         }
-        System.out.println(Arrays.toString(arr3));
+        System.out.println(c);
     }
 }
