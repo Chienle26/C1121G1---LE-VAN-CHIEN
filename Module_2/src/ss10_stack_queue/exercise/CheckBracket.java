e5f177b10aa62d3106657105960437314c04d51c
C1121G1-LE-VAN-CHIEN <chienle26897@gmail.com>
2022-01-13 16:11:12 +0700

    Update BT

---
+++ Module_2/src/ss10_stack_queue/exercise/CheckBracket.java
@@ -0,0 +1,40 @@
+package ss10_stack_queue.exercise;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class CheckBracket {
+    public static void main(String[] args) {
+        while (true){
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Nhập chuỗi cần kiểm tra Backet: ");
+            String string = scanner.nextLine();
+            Stack<String> bStack = new Stack<>();
+//        Queue<String> queue = new LinkedList<>();
+            String[] arrString = string.split("");
+            Boolean check = true;
+            for (String element : arrString) {
+                if (element.equals("(")) {
+                    bStack.push(element);
+                } else if (element.equals(")")) {
+                    if (bStack.isEmpty()) {
+                        check = false;
+                        break;
+                    } else if (bStack.pop().equals("(")) {
+                        check = true;
+                    }
+                }
+            }
+
+            if (check && bStack.isEmpty()) {
+                System.out.println("Well");
+            } else {
+                System.out.println("Wrong");
+            }
+        }
+
+
+    }
+}
