b14115b75c6b01d2086884640b2947589c9c34e5
C1121G1-LE-VAN-CHIEN <chienle26897@gmail.com>
2022-01-21 15:41:24 +0700

    Update BT

--- Module_2/src/ss15_io_text_file/excercise/copy_file_text/CopyFileText.java
+++ Module_2/src/ss15_io_text_file/excercise/copy_file_text/CopyFileText.java
@@ -2,12 +2,36 @@ package ss15_io_text_file.excercise.copy_file_text;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class CopyFileText {
-    public static void readAndWriteFileText(String pathFileSource, String pathFileTarget) {
+    public static int readAndWriteFileText(String pathFileSource, String pathFileTarget) {
         File fileSource = new File(pathFileSource);
         File fileTarget = new File(pathFileTarget);
+        int count = 0;
+        //Cách 1: Dùng FileRead-Writer để đọc ghi từng ký tự
         try {
+            FileReader fileReader = new FileReader(fileSource);
+            FileWriter fileWriter = new FileWriter(fileTarget, false);
+            int character;
+            String temp="";
+            while ((character = fileReader.read()) != -1) {
+                count++;
+                temp+=(char)character;
+                fileWriter.write((char) character);
+            }
+            fileReader.close();
+            fileWriter.close();
+//            String[] strings = temp.split("\n");
+
+        } catch (IOException e) {
+            System.err.println("File bị lỗi!");
+        }
+        return count;
+
+
+        //Cách 2: Dùng BufferedReader-Writer để đọc-ghi từng dòng
+        /*try {
             FileReader fileReader = new FileReader(fileSource);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             FileWriter fileWriter = new FileWriter(fileTarget,false);
@@ -23,18 +47,18 @@ public class CopyFileText {
             fileReader.close();
         } catch (IOException e) {
             System.err.println("Lỗi đọc file");
-        }
+        }*/
     }
 
     public static void main(String[] args) {
-        CopyFileText.readAndWriteFileText("src/ss15_io_text_file/excercise/copy_file_text/SourceFile.csv",
+        int chars = CopyFileText.readAndWriteFileText("src/ss15_io_text_file/excercise/copy_file_text/SourceFile.csv",
                 "src/ss15_io_text_file/excercise/copy_file_text/TargetFile.csv");
         try {
             File fileTarget = new File("src/ss15_io_text_file/excercise/copy_file_text/TargetFile.csv");
             FileReader fileReader = new FileReader(fileTarget);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line = "";
-            while ((line=bufferedReader.readLine()) != null) {
+            while ((line = bufferedReader.readLine()) != null) {
                 System.out.println(line);
             }
             bufferedReader.close();
@@ -42,5 +66,6 @@ public class CopyFileText {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println("Số ký tự trong tệp là: " + chars);
     }
 }
