78163f29523fc60eefebe23b2b962049474d1e88
C1121G1-LE-VAN-CHIEN <chienle26897@gmail.com>
2022-02-18 17:28:17 +0700

    Update BT

--- Module_2/src/ss_case_study_furama_resort/utils/BookingComparator.java
+++ Module_2/src/ss_case_study_furama_resort/utils/BookingComparator.java
@@ -11,7 +11,7 @@ import java.util.Date;
 public class BookingComparator implements Comparator<Booking> {
     @Override
     public int compare(Booking o1, Booking o2) {
-        int date=1;
+        int date = 1;
         Calendar cal = Calendar.getInstance();
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         try {
@@ -32,3 +32,28 @@ public class BookingComparator implements Comparator<Booking> {
         return date;
     }
 }
+
+//Cách 2
+//public class BookingComparator implements Comparator<Booking> {
+//    @0verride
+//    public int compare(Booking o1, Booking o2) {
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/HM/yyyy");
+//        LocalDate startDatel = LocalDate.parse(o1.getStartDate(), formatter);
+//        LocalDate startDate2 = LocalDate.parse(o2.getStartDate(), formatter);
+//        LocalDate endDatel = LocalDate.parse(o2.getEndDate(), formatter);
+//        LocalDate endDate2 = LocalDate.parse(o2.getEndDate(), formatter);
+//        if (startDatel.compareTo(startDate2) > 0) {
+//            return 1;
+//        } else if (startDatel.compareTo(startDate2) < 0) {
+//            return -1;
+//        } else {
+//            if (endDatel.compareTo(endDate2) > 0) {
+//                return 1;
+//            } else if (endDatel.compareTo(endDate2) < 0) {
+//                return -1;
+//            } else {
+//                return 0;
+//            }
+//        }
+//    }
+//}
