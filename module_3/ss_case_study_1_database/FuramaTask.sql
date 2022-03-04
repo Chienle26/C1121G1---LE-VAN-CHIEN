633c204dd8ddf6ebc74e95f3d08cb7956a854708
C1121G1-LE-VAN-CHIEN <chienle26897@gmail.com>
2022-03-04 16:19:11 +0700

    Update BT

--- module_3/ss_case_study_1_database/FuramaTask.sql
+++ module_3/ss_case_study_1_database/FuramaTask.sql
@@ -17,15 +17,35 @@ group by khach_hang.ma_khach_hang
 order by so_lan_dat_phong asc;
 
 -- Task 5:
-select khach_hang.ma_khach_hang, khach_hang.ho_ten, loai_khach.ten_loai_khach, hop_dong.ma_hop_dong, dich_vu.ten_dich_vu, hop_dong.ngay_lam_hop_dong, hop_dong.ngay_ket_thuc, 
-sum(ifnull(dich_vu.chi_phi_thue,0) + ifnull(hop_dong_chi_tiet.so_luong,0) * ifnull(dich_vu_di_kem.gia,0)) as 'tong_tien' from khach_hang
-left join loai_khach on loai_khach.ma_loai_khach = khach_hang.ma_loai_khach
-left join hop_dong on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
-left join dich_vu on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
-left join hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+-- select khach_hang.ma_khach_hang, khach_hang.ho_ten, loai_khach.ten_loai_khach, hop_dong.ma_hop_dong, dich_vu.ten_dich_vu, hop_dong.ngay_lam_hop_dong, hop_dong.ngay_ket_thuc, 
+-- sum(ifnull(dich_vu.chi_phi_thue,0) + ifnull(hop_dong_chi_tiet.so_luong,0) * ifnull(dich_vu_di_kem.gia,0)) as 'tong_tien' from khach_hang
+-- left join loai_khach on loai_khach.ma_loai_khach = khach_hang.ma_loai_khach
+-- left join hop_dong on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+-- left join dich_vu on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+-- left join hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+-- left join dich_vu_di_kem on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+-- group by hop_dong.ma_hop_dong
+-- order by ma_khach_hang, ma_hop_dong desc;
+
+create temporary table bang_tam_tinh_tong_tien_dvdk
+select hop_dong.ma_khach_hang, hop_dong.ma_hop_dong, hop_dong.ngay_lam_hop_dong, hop_dong.ngay_ket_thuc, hop_dong.ma_dich_vu,
+(sum(ifnull(hop_dong_chi_tiet.so_luong,0) * ifnull(dich_vu_di_kem.gia,0)) + ifnull(dich_vu.chi_phi_thue,0)) as tong_tien
+from dich_vu 
+inner join hop_dong on dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
+left join hop_dong_chi_tiet on hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong
 left join dich_vu_di_kem on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
 group by hop_dong.ma_hop_dong
-order by ma_khach_hang, ma_hop_dong desc;
+order by hop_dong.ma_hop_dong;
+
+select khach_hang.ma_khach_hang, loai_khach.ten_loai_khach, khach_hang.ho_ten, bang_tam_tinh_tong_tien_dvdk.ma_hop_dong, dich_vu.ten_dich_vu, 
+bang_tam_tinh_tong_tien_dvdk.ngay_lam_hop_dong, bang_tam_tinh_tong_tien_dvdk.ngay_ket_thuc,
+sum(ifnull(bang_tam_tinh_tong_tien_dvdk.tong_tien,0)) as tong_chi_phi
+from dich_vu
+inner join bang_tam_tinh_tong_tien_dvdk on bang_tam_tinh_tong_tien_dvdk.ma_dich_vu = dich_vu.ma_dich_vu
+right join khach_hang on khach_hang.ma_khach_hang = bang_tam_tinh_tong_tien_dvdk.ma_khach_hang
+right join loai_khach on loai_khach.ma_loai_khach = khach_hang.ma_loai_khach
+group by khach_hang.ma_khach_hang
+order by khach_hang.ma_khach_hang;
 
 -- Task 6:
 select dich_vu.ma_dich_vu, dich_vu.ten_dich_vu, dich_vu.dien_tich, dich_vu.chi_phi_thue, loai_dich_vu.ten_loai_dich_vu 
@@ -39,8 +59,8 @@ order by dien_tich desc;
 -- Task 7:
 select dich_vu.ma_dich_vu, dich_vu.ten_dich_vu, dich_vu.dien_tich,dich_vu.so_nguoi_toi_da, dich_vu.chi_phi_thue, loai_dich_vu.ten_loai_dich_vu 
 from loai_dich_vu
-left join dich_vu on loai_dich_vu.ma_loai_dich_vu = dich_vu.ma_loai_dich_vu
-left join hop_dong on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+join dich_vu on loai_dich_vu.ma_loai_dich_vu = dich_vu.ma_loai_dich_vu
+join hop_dong on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
 where (dich_vu.ma_dich_vu in (select hop_dong.ma_dich_vu from hop_dong where year(ngay_lam_hop_dong) = 2020)) 
 and (dich_vu.ma_dich_vu not in (select hop_dong.ma_dich_vu from hop_dong where year(ngay_lam_hop_dong) = 2021))
 group by dich_vu.ma_dich_vu
