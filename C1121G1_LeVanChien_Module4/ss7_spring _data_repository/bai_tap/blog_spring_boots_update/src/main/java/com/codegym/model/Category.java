83ef871827f234fc0dc4609334b5c4864f28f106
C1121G1-LE-VAN-CHIEN <chienle26897@gmail.com>
2022-04-06 18:50:06 +0700

    Update BT

---
+++ C1121G1_LeVanChien_Module4/ss7_spring _data_repository/bai_tap/blog_spring_boots_update/src/main/java/com/codegym/model/Category.java
@@ -0,0 +1,43 @@
+package com.codegym.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String categoryName;
+
+    @OneToMany (mappedBy = "category")
+    private List<Blog> blogList;
+
+    public Category() {
+    }
+
+    public List<Blog> getBlogList() {
+        return blogList;
+    }
+
+    public void setBlogList(List<Blog> blogList) {
+        this.blogList = blogList;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
---
+++ C1121G1_LeVanChien_Module4/ss7_spring _data_repository/bai_tap/blog_spring_boots_update/src/main/java/com/codegym/repository/IBlogRepository.java
@@ -0,0 +1,14 @@
+package com.codegym.repository;
+
+import com.codegym.model.Blog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+public interface IBlogRepository extends JpaRepository<Blog,Integer> {
+    Page<Blog> findByAuthorContaining(String name, Pageable pageable);
+}
