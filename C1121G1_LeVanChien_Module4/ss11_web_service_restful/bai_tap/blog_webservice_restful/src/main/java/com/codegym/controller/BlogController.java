a071d629d16caa40f61931c188b9f9e558f3409f
C1121G1-LE-VAN-CHIEN <chienle26897@gmail.com>
2022-04-14 16:19:41 +0700

    Update BT

---
+++ C1121G1_LeVanChien_Module4/ss11_web_service_restful/bai_tap/blog_webservice_restful/src/main/java/com/codegym/controller/BlogController.java
@@ -0,0 +1,91 @@
+package com.codegym.controller;
+
+import com.codegym.model.Blog;
+import com.codegym.service.IBlogService;
+import com.codegym.service.ICategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(("/blog"))
+public class BlogController {
+
+    @Autowired
+    IBlogService iBlogService;
+
+    @Autowired
+    ICategoryService iCategoryService;
+
+    @GetMapping({"", "/list"})
+    public ResponseEntity<Page<Blog>> goBlogList(@PageableDefault(value = 3) Pageable pageable, @RequestParam Optional<String> search) {
+        String keyword = search.orElse("");
+        Page<Blog> blogs = iBlogService.findAllPaging(keyword, pageable);
+        return new ResponseEntity<>(blogs, HttpStatus.OK);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Blog>> findByCategoryName(@RequestParam String categoryName) {
+        List<Blog> blogs = iBlogService.findByCategory(categoryName);
+        return new ResponseEntity<>(blogs, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}/view")
+    public ResponseEntity<Blog> goViewBlog(@PathVariable Integer id) {
+        Blog blog = iBlogService.findById(id);
+        return new ResponseEntity<>(blog, HttpStatus.OK);
+    }
+
+    @GetMapping("/create")
+    public String goCreateBlog(Model model) {
+        model.addAttribute("blog", new Blog());
+        model.addAttribute("categoryList", iCategoryService.findAll());
+        return "blog/create";
+    }
+
+    @PostMapping("/save")
+    public String saveBlog(Blog blog) {
+        blog.setDate(String.valueOf((LocalDate.now())));
+        iBlogService.save(blog);
+        return "redirect:/blog/list";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String goEditBlog(@PathVariable Integer id, Model model) {
+        model.addAttribute("blog", iBlogService.findById(id));
+        model.addAttribute("categoryList", iCategoryService.findAll());
+        return "blog/edit";
+    }
+
+    @PostMapping("/update")
+    public String updateBlog(Blog blog, RedirectAttributes redirectAttributes) {
+        blog.setDate(String.valueOf(LocalDate.now()));
+        iBlogService.save(blog);
+        redirectAttributes.addFlashAttribute("success", "Chỉnh sửa blog thành công!");
+        return "redirect:/blog/list";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String goDeleteBlog(Model model, @PathVariable Integer id) {
+        model.addAttribute("blog", iBlogService.findById(id));
+        return "blog/delete";
+    }
+
+    @PostMapping("/delete")
+    public String deleteBlog(Blog blog, RedirectAttributes redirectAttributes) {
+        iBlogService.remove(blog.getId());
+        redirectAttributes.addFlashAttribute("success", "Xóa thành công!");
+        return "redirect:/blog/list";
+    }
+}
---
+++ C1121G1_LeVanChien_Module4/ss11_web_service_restful/bai_tap/blog_webservice_restful/src/main/java/com/codegym/repository/IBlogRepository.java
@@ -0,0 +1,25 @@
+package com.codegym.repository;
+
+import com.codegym.model.Blog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Transactional
+public interface IBlogRepository extends JpaRepository<Blog,Integer> {
+    Page<Blog> findByAuthorContainingOrderByDate(String name, Pageable pageable);
+
+//    List<Blog> findAllByCategory_CategoryNameContaining(String categoryName);
+
+    
+
+    @Query(value = "select b from blog b where b.category.categoryName like concat('%',:categoryName,'%')")
+    List<Blog>findAllByCategoryName(@Param("categoryName") String categoryName);
+}
