2daebe97578431d1170589b7bcdf366c1942eb98
C1121G1-LE-VAN-CHIEN <chienle26897@gmail.com>
2022-05-10 20:47:37 +0700

    Update BT

--- C1121G1_LeVanChien_Module5/ss_case_study/casestudy-angular/src/app/app.module.ts
+++ C1121G1_LeVanChien_Module5/ss_case_study/casestudy-angular/src/app/app.module.ts
@@ -4,12 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ListServiceComponent } from './list-service/list-service.component';
+import { EditServiceComponent } from './edit-service/edit-service.component';
+import { CreateServiceComponent } from './create-service/create-service.component';
+import { ListCustomerComponent } from './list-customer/list-customer.component';
+import { EditCustomerComponent } from './edit-customer/edit-customer.component';
+import { CreateCustomerComponent } from './create-customer/create-customer.component';
+import { ListContractComponent } from './list-contract/list-contract.component';
+import { CreateContractComponent } from './create-contract/create-contract.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    ListServiceComponent,
+    EditServiceComponent,
+    CreateServiceComponent,
+    ListCustomerComponent,
+    EditCustomerComponent,
+    CreateCustomerComponent,
+    ListContractComponent,
+    CreateContractComponent
   ],
   imports: [
     BrowserModule
