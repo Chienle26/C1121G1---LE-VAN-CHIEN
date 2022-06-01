5cc93a63d9c823770d1e55aae7dfa4061dfe9004
C1121G1-LE-VAN-CHIEN <chienle26897@gmail.com>
2022-06-01 18:48:09 +0700

    up

--- module_6/sprint-1/angular/sprint_1/sprint-c1121g1/src/app/app-routing.module.ts
+++ module_6/sprint-1/angular/sprint_1/sprint-c1121g1/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomepageRoutingModule} from './component/homepage/homepage-routing.module';
 import {HomepageModule} from './component/homepage/homepage.module';
+import {ProductListBestsellerComponent} from './component/homepage/product-list-bestseller/product-list-bestseller.component';
 
 
 const routes: Routes = [
-  // {path: '', component: HomepageModule}
+  {path: '', component: ProductListBestsellerComponent}
 ];
 
 @NgModule({
